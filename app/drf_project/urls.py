49aba8e06a1dbc920ef628322bd0be36ad80f6df
Kamaro <kamaroly@gmail.com>
2022-09-12 15:43:29 +0300

    Can create movie via API

--- app/drf_project/urls.py
+++ app/drf_project/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import ping
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ping/', ping, name="ping")
+    path('ping/', ping, name="ping"),
+    path("", include("movies.urls")),
 ]
