4ba36653e85cf3feebd2eaeddbc15d5560ca10a3
Kamaro <kamaroly@gmail.com>
2022-09-12 16:18:21 +0300

    Can fetch all movies

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -63,14 +63,25 @@ def test_add_movies_invalid_json_keys(client):
     assert len(movies) == 0
     
 @pytest.mark.django_db
-def test_get_single_movie(client):
-    movie = Movie.objects.create(title="The Book Of Eli", genre= "Biography", year="2009")
+def test_get_single_movie(client, add_movie):
+    
+    movie = add_movie(title="The Book Of Eli", genre= "Biography", year="2009")
     response = client.get(f"/api/movies/{movie.id}/")
     
     assert response.status_code == 200
     assert response.data["title"] == "The Book Of Eli"
-    
+
 def test_get_movie_incorrect_id(client):
     response = client.get(f"/api/movies/foo/")
     assert response.status_code == 404
 
+@pytest.mark.django_db
+def test_get_all_movies(client, add_movie):
+    movie_one= add_movie(title="The Book Of Eli", genre="True Movie", year="2019")
+    movie_two= add_movie(title="Never Split the difference", genre="Biograph", year="2018")
+    
+    response = client.get(f"/api/movies/")
+    
+    assert response.status_code == 200
+    assert response.data[0]["title"] == movie_one.title
+    assert response.data[1]["title"] == movie_two.title
