bdc2efeaee8c3605d3b838b0afa8ae9461c72a3a
Kamaro <kamaroly@gmail.com>
2022-09-12 15:51:57 +0300

    Added invalid requests tests

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -7,7 +7,6 @@ from movies.models import Movie
 
 def test_add_movie(client):
     movies = Movie.objects.all()
-    
     assert len(movies) == 0
     
     # When
@@ -20,12 +19,48 @@ def test_add_movie(client):
         },
         content_type="application/json"
     )
-    
     # Then
     assert response.status_code == 201
     assert response.data["title"] == "The Big Lebowski"
     
     movies = Movie.objects.all()
     assert len(movies) == 1
+    
+@pytest.mark.django_db
+def test_add_movie_invalid_json(client):
+    movies = Movie.objects.all()
+    
+    assert len(movies) == 0
+    
+    response = client.post(
+        "/api/movies/",
+        {},
+        content_type="application/json"
+    )
+    
+    assert response.status_code == 400 
+    
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+    
+@pytest.mark.django_db
+def test_add_movies_invalid_json_keys(client):
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+    
+    response = client.post(
+        "/api/movies/",
+        {
+            "title": "The Book Of Eli",
+            "genre": "comedy",
+        },
+        content_type="application/json"
+    )
+    
+    assert response.status_code == 400
+    
+    movies = Movie.objects.all()
+    assert len(movies) == 0
+    
     
     
